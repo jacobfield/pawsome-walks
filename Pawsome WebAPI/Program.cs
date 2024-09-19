fd8381cc90fda28b827533aae19a765fbae8337d
jacobfield <jacobprogrammes@gmail.com>
2024-09-19 10:11:26 +0100

    dependencies initial commit

--- Pawsome WebAPI/Program.cs
+++ Pawsome WebAPI/Program.cs
@@ -29,6 +29,7 @@ app.MapGet("/weatherforecast", () =>
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
+            
         ))
         .ToArray();
     return forecast;
