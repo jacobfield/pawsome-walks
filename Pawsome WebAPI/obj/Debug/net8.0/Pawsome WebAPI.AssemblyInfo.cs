cc5e2915f33e359f92f93b33162b1eab49cf8ef2
jacobfield <jacobprogrammes@gmail.com>
2024-10-09 14:30:54 +0100

    data.json update to include array walkType data

--- Pawsome WebAPI/obj/Debug/net8.0/Pawsome WebAPI.AssemblyInfo.cs
+++ Pawsome WebAPI/obj/Debug/net8.0/Pawsome WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pawsome WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abdefc69ce5ef8e67cec670e523448f632aeb33c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc1c9e82d82be5778caad1288dc11af4c06e30f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pawsome WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pawsome WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
