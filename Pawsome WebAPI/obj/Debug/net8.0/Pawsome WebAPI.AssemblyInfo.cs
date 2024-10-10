fd62663c69c9c92c88f94274e0b7bf7736c8b001
jacobfield <jacobprogrammes@gmail.com>
2024-10-10 11:00:13 +0100

    development branch initial commit!

--- Pawsome WebAPI/obj/Debug/net8.0/Pawsome WebAPI.AssemblyInfo.cs
+++ Pawsome WebAPI/obj/Debug/net8.0/Pawsome WebAPI.AssemblyInfo.cs
@@ -13,8 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pawsome WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc1c9e82d82be5778caad1288dc11af4c06e30f")]
-
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4710d172f78160af62824907e3950e287fba9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pawsome WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pawsome WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
