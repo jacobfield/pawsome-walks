c0b2b25f5b4ff526547ba23e7931fe17c5080db4
jacobfield <jacobprogrammes@gmail.com>
2024-10-14 10:54:47 +0100

    create owners table .js added

--- pawsome-walks/database/reset-database/create/create-owners-table.js
+++ pawsome-walks/database/reset-database/create/create-owners-table.js
@@ -1,3 +1,20 @@
 import { pool } from "../../../index.js";
 
 // function to create the initial tables
+export async function createOwnersTable() {
+  try {
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS owners (
+            ownerId SERIAL PRIMARY KEY,
+            userName TEXT NOT NULL,
+            email TEXT NOT NULL,
+            hashedPassword TEXT NOT NULL
+            )`);
+    console.log("owners table created");
+  } catch (error) {
+    console.error(
+      "owners table not created. Error originated in create-owners-table.js",
+      error
+    );
+  }
+}
