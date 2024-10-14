840a6859e98a58e02e33d6c3feabb10467d3e6b3
jacobfield <jacobprogrammes@gmail.com>
2024-10-14 10:28:25 +0100

    helper functions folders created. Create Owner helper function added

---
+++ pawsome-walks/helper-functions/owners-table/createOwner.js
@@ -0,0 +1,26 @@
+import { pool } from "../../index.js";
+
+// create function to add a new owner to the owners table
+
+export async function createOwner(owner) {
+  try {
+    const insertOwner = `INSERT INTO owners (ownerId, userName, email, hashedPassword) VALUES ($1, $2, $3, $4) RETURNING *`;
+    // destructure the owner object
+    const { ownerId, userName, email, hashedPassword } = owner;
+
+    // use pool object to send query to the database, preventing SQL injection
+    const result = await pool.query(insertOwner, [
+      ownerId,
+      userName,
+      email,
+      hashedPassword,
+    ]);
+    // the rows property should contain the new owner
+    return result.rows[0];
+  } catch (error) {
+    console.error(
+      `Error creating new owner. Error originated in createOwner.js. Error: ${error}`
+    );
+    throw error;
+  }
+}
