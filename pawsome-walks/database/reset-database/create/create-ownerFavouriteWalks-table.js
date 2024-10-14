a19e92d6deaa0ce4194bcc21edc99a36377cf029
jacobfield <jacobprogrammes@gmail.com>
2024-10-14 13:52:29 +0100

    create db functions added into create functions file

---
+++ pawsome-walks/database/reset-database/create/create-ownerFavouriteWalks-table.js
@@ -0,0 +1,22 @@
+import { pool } from "../../../index.js";
+
+// Function to create ownerFavouriteWalks table
+export default async function createOwnerFavouriteWalks() {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS ownerFavouriteWalks (
+        ownerId INT NOT NULL,
+        walkId INT NOT NULL,
+        CONSTRAINT fk_ownerId FOREIGN KEY (ownerId) REFERENCES owners(ownerId),
+        CONSTRAINT fk_walkId FOREIGN KEY (walkId) REFERENCES walks(walkId),
+        PRIMARY KEY (ownerId, walkId)  -- Composite Primary Key
+      );
+    `);
+    console.log("ownerFavouriteWalks table created");
+  } catch (error) {
+    console.error(
+      "ownerFavouriteWalks table not created. Error originated in create-ownerFavouriteWalks-table.js",
+      error
+    );
+  }
+}
---
+++ pawsome-walks/database/reset-database/create/createFunctions.js
@@ -0,0 +1,19 @@
+import { createOwnersTable } from "./createOwnersTable.js";
+import { createDogsTable } from "./createDogsTable.js";
+import { createWalksTable } from "./createWalksTable.js";
+import { createOwnersDogsTable } from "./createOwnersDogsTable.js";
+import { createOwnerFavouriteWalks } from "./createOwnerFavouriteWalks.js";
+import { createDogPalRequestsTable } from "./createDogPalRequestsTable.js";
+import { createDogPalsTable } from "./createDogPalsTable.js";
+import { createWalkCommentsTable } from "./createWalkCommentsTable.js";
+
+export const createFunctions = {
+  createOwnersTable,
+  createDogsTable,
+  createWalksTable,
+  createOwnersDogsTable,
+  createOwnerFavouriteWalks,
+  createDogPalRequestsTable,
+  createDogPalsTable,
+  createWalkCommentsTable,
+};
