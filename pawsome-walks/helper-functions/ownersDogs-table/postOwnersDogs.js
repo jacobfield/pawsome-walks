281231014fd67707dd102de1a444096cc8b819c6
jacobfield <jacobprogrammes@gmail.com>
2024-10-15 10:46:07 +0100

    post owners dogs

---
+++ pawsome-walks/helper-functions/ownersDogs-table/postOwnersDogs.js
@@ -0,0 +1,18 @@
+import { pool } from "../../index.js";
+
+export default async function postOwnersDogs(ownerId, dogId) {
+  try {
+    const postOwnersDogs =
+      "INSERT INTO ownersDogs (ownerId, dogId) VALUES ($1, $2) RETURNING *";
+
+    const result = await pool.query(postOwnersDogs, [ownerId, dogId]);
+
+    return result.rows[0];
+  } catch (error) {
+    console.error(
+      "Error creating new ownersDogs. Error originated in postOwnersDogs.js",
+      error
+    );
+    throw error;
+  }
+}
