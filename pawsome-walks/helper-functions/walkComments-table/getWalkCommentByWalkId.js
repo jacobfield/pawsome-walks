66d28be9219c277d70d421809733ffae2b984c7d
jacobfield <jacobprogrammes@gmail.com>
2024-10-15 12:29:22 +0100

    walk comments table helper functions added. That should be all helper functions added.

---
+++ pawsome-walks/helper-functions/walkComments-table/getWalkCommentByWalkId.js
@@ -0,0 +1,19 @@
+import { pool } from "../../index.js";
+
+export default async function getWalkCommentByWalkId(walkId) {
+  try {
+    if (!walkId) {
+      throw new Error("walkId is required.");
+    }
+
+    const walkCommentQuery = `SELECT * FROM walkComments WHERE walkId = $1`;
+    const result = await pool.query(walkCommentQuery, [walkId]);
+    return result.rows;
+  } catch (error) {
+    console.error(
+      "Error fetching walkComments by walkId. Error originated in getWalkCommentByWalkId.js",
+      error
+    );
+    throw error;
+  }
+}
