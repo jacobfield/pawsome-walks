9a6125cb7bbdbcd1e200d01b43adfc7cf5bcad7c
jacobfield <jacobprogrammes@gmail.com>
2024-10-15 10:56:11 +0100

    post owners favourite walks

---
+++ pawsome-walks/helper-functions/ownerFavouriteWalks-table/postOwnerFavouriteWalks.js
@@ -0,0 +1,16 @@
+import { pool } from "../../index.js";
+
+export default async function postOwnerFavouriteWalks(ownerId, walkId) {
+  try {
+    const postOwnerFavouriteWalks =
+      "INSERT INTO ownerFavouriteWalks (ownerId, walkId) VALUES ($1, $2) RETURNING *";
+    const result = await pool.query(postOwnerFavouriteWalks, [ownerId, walkId]);
+    return result.rows[0];
+  } catch (error) {
+    console.error(
+      "Error creating new ownerFavouriteWalks. Error originated in postOwnerFavouriteWalks.js",
+      error
+    );
+    throw error;
+  }
+}
