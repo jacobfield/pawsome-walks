f97638cf2f18d40d2baa3cfdf015fe9b0d605b9c
jacobfield <jacobprogrammes@gmail.com>
2024-10-15 15:35:12 +0100

    owner favourite walks controllers added

---
+++ pawsome-walks/controllers/ownerFavouriteWalks-controllers/getOwnerFavouriteWalksByOwnerIdController.js
@@ -0,0 +1,37 @@
+import { ownerFavouriteWalks } from "../../helper-functions/helperFunctions.js";
+
+export default async function getOwnerFavouriteWalksByOwnerIdController(
+  req,
+  res
+) {
+  try {
+    const id = req.params.ownerId;
+    if (!id) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "Owner ID is required" });
+    }
+    const ownerFavouriteWalksByOwnerId =
+      await ownerFavouriteWalks.getOwnerFavouriteWalksByOwnerId(id);
+
+    if (!ownerFavouriteWalksByOwnerId) {
+      return res.status(404).json({
+        status: "error",
+        message: "No favourite walks found with that Owner ID",
+      });
+    }
+
+    res
+      .status(200)
+      .json({ status: "success", data: ownerFavouriteWalksByOwnerId });
+  } catch (error) {
+    console.error(
+      "Error in getOwnerFavouriteWalksByOwnerIdController: ",
+      error
+    );
+    res.status(500).json({
+      status: "error",
+      message: "Error getting favourite walks by Owner ID",
+    });
+  }
+}
--- pawsome-walks/helper-functions/ownerFavouriteWalks-table/deleteOwnerFavouriteWalksByOwnerId.js
+++ pawsome-walks/helper-functions/ownerFavouriteWalks-table/deleteOwnerFavouriteWalksByOwnerId.js
@@ -1,14 +1,20 @@
 import { pool } from "../../index.js";
 
-export default async function deleteOwnerFavouriteWalksByOwnerId(ownerId) {
+export default async function deleteOwnerFavouriteWalksByOwnerId(
+  ownerId,
+  walkId
+) {
   try {
     const deleteOwnerFavouriteWalks =
-      "DELETE FROM ownerFavouriteWalks WHERE ownerId = $1 RETURNING *";
-    const result = await pool.query(deleteOwnerFavouriteWalks, [ownerId]);
+      "DELETE FROM ownerFavouriteWalks WHERE ownerId = $1 AND walkId = $2 RETURNING *";
+    const result = await pool.query(deleteOwnerFavouriteWalks, [
+      ownerId,
+      walkId,
+    ]);
     return result.rows;
   } catch (error) {
     console.error(
-      "Error deleting ownerFavouriteWalks by ownerId. Error originated in deleteOwnerFavouriteWalksByOwnerId.js",
+      "Error deleting ownerFavouriteWalks by ownerId and walkId. Error originated in deleteOwnerFavouriteWalksByOwnerId.js",
       error
     );
     throw error;
