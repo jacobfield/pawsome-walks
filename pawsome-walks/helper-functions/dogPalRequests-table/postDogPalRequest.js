ca43adc227e3440281f098f75b4f6c20a3af1415
jacobfield <jacobprogrammes@gmail.com>
2024-10-16 15:28:35 +0100

    fixed post dog pal request

--- pawsome-walks/helper-functions/dogPalRequests-table/postDogPalRequest.js
+++ pawsome-walks/helper-functions/dogPalRequests-table/postDogPalRequest.js
@@ -1,20 +1,18 @@
 import { pool } from "../../index.js";
 
-export default async function postDogPalRequest(
+export default async function postDogPalRequest({
   senderDogId,
   receiverDogId,
-  status
-) {
+}) {
   try {
-    if (senderDogId == receiverDogId) {
+    if (senderDogId === receiverDogId) {
       throw new Error("You can't send a dogPal request to yourself!");
     }
-    const postDogPalRequest = `INSERT INTO dogPalRequests (senderDogId, receiverDogId, status)  VALUES ($1, $2, $3) RETURNING *`;
+    const postDogPalRequest = `INSERT INTO dogPalRequests (senderDogId, receiverDogId)  VALUES ($1, $2) RETURNING *`;
 
     const result = await pool.query(postDogPalRequest, [
       senderDogId,
       receiverDogId,
-      status,
     ]);
     return result.rows[0];
   } catch (error) {
