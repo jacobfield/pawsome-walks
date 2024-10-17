e607367cdd954eacb1be6832fc096cad1c2ca602
jacobfield <jacobprogrammes@gmail.com>
2024-10-17 15:47:09 +0100

    dark theme working with toggle function. About to exchange toggle to button

--- pawsome-walks/src/comps/AppContainer.jsx
+++ pawsome-walks/src/comps/AppContainer.jsx
@@ -3,43 +3,11 @@ import { useContext } from "react";
 import { ThemeContext } from "./ThemeProvider";
 
 export default function AppContainer({ children }) {
-  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
-
-  const themeStyles = darkTheme
-    ? {
-        backgroundColor: "#f8f9fa",
-        color: "#343a40",
-      }
-    : {
-        backgroundColor: "#343a40",
-        color: "#f8f9fa",
-      };
+  const { darkTheme } = useContext(ThemeContext); // Only need darkTheme to apply styles
 
   return (
-    <div
-      className="AppContainer"
-      style={{
-        ...themeStyles,
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <hr style={{ visibility: "hidden" }} />
-      <div className="form">
-        <input
-          className="formInput"
-          type="checkbox"
-          onChange={() => setDarkTheme(!darkTheme)}
-          id="themeToggle"
-        />
-        <label htmlFor="themeToggle" className="switch">
-          <span className={`icon ${!darkTheme ? "icon-right" : ""}`}>
-            {!darkTheme ? "🌛" : "🌞"}
-          </span>
-        </label>
-      </div>
+    <div className={`AppContainer ${darkTheme ? "dark" : "light"}`}>
+      {/* No need for the form and toggle here */}
       <div className="mapContainer">{children}</div>
     </div>
   );
--- pawsome-walks/src/comps/NavBar.jsx
+++ pawsome-walks/src/comps/NavBar.jsx
@@ -1,11 +1,29 @@
 import { CiStar } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
-import { useState } from "react";
+import { GiNightSleep } from "react-icons/gi"; // <GiNightSleep />
+import { GiNightSky } from "react-icons/gi"; // <GiNightSky />
+import { GiHeraldicSun } from "react-icons/gi"; // <GiHeraldicSun />
+import { useState, useContext } from "react";
+import { ThemeContext } from "./ThemeProvider";
+//
 export default function NavBar() {
   const [profilePicture, setProfilePicture] = useState(null);
+  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
   return (
     <div className="navBar">
-      <div className="darkMode"></div>
+      <div className="darkMode">
+        <input
+          className="formInput"
+          type="checkbox"
+          onChange={() => setDarkTheme(!darkTheme)}
+          id="themeToggle"
+        />
+        <label htmlFor="themeToggle" className="switch">
+          <span className={`icon ${!darkTheme ? "icon-right" : ""}`}>
+            {!darkTheme ? "🌛" : "🌞"}
+          </span>
+        </label>
+      </div>
       <CiStar className="starIcon icon" />
       {!profilePicture ? (
         <CgProfile className="profileIcon icon" />
