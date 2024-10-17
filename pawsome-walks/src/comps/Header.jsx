25583e3248f38f9bec5799c8a44fe18589f58dfc
jacobfield <jacobprogrammes@gmail.com>
2024-10-17 12:05:15 +0100

    dynamic logo sizing

--- pawsome-walks/src/comps/Header.jsx
+++ pawsome-walks/src/comps/Header.jsx
@@ -1,7 +1,12 @@
 export default function Header() {
   return (
     <header className="header">
-      <img className="logo"></img> {/* flex-start left*/}
+      <img
+        className="logo"
+        alt="Pawsome Walks Logo"
+        src="../../public/logo.jpg"
+      ></img>
+      {/* flex-start left*/}
       <div className="searchBar"></div> {/* flex-start center*/}
       <nav className="navBar"></nav> {/* flexstart right*/}
     </header>
