a5097435b9af5aaa594dad4b6781ceb2d766da5d
jacobfield <jacobprogrammes@gmail.com>
2024-10-17 11:53:54 +0100

    removed root max width constrict

--- pawsome-walks/src/App.jsx
+++ pawsome-walks/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "./styles/small.css";
 import "./styles/medium.css";
 import "./styles/large.css";
+import "./styles/index.css";
 import Header from "./comps/Header.jsx";
 function App() {
   return (
--- pawsome-walks/src/main.jsx
+++ pawsome-walks/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/index.css";
 import "./App.css";
 import "./styles/small.css";
 import "./styles/medium.css";
