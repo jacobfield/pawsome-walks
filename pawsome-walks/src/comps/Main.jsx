d0c55fe169cd7b1da4b2d508667498e83e24dc0e
jacobfield <jacobprogrammes@gmail.com>
2024-10-18 15:58:06 +0100

    walks containers centred properly, with api getAllWalks call mapping through names and images, dynamically rendering them in the middle of the vp width.

--- pawsome-walks/src/comps/Main.jsx
+++ pawsome-walks/src/comps/Main.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
-export default function Main() {
+export default function Main({ allWalks }) {
   const [walks, setWalks] = useState(
     Array.from({ length: 30 }, (_, i) => i + 1)
   );
   //setWalks
   return (
     <section className="walksContainer">
+      {allWalks &&
+        allWalks.map((walk) => (
+          <div key={walk.walkid} className="walk">
+            <h2>{walk.walkname}</h2>
+            <img
+              className="walkPreviewImg"
+              src={`walk-photos/${walk.photopath}.jpg`}
+              alt={walk.walkname}
+            />
+          </div>
+        ))}
+      {/* ))}
       {walks.map((walk) => (
         <div key={walk} className="walk"></div>
-      ))}
+      ))} */}
     </section>
   );
 }
