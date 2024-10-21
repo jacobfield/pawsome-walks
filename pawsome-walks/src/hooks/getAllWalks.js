723160be5b90ab10f2d612c954ba9f8f4c02affd
jacobfield <jacobprogrammes@gmail.com>
2024-10-21 10:26:38 +0100

    getAllWalks.js route update

--- pawsome-walks/src/hooks/getAllWalks.js
+++ pawsome-walks/src/hooks/getAllWalks.js
@@ -1,6 +1,6 @@
 export default async function getAllWalks() {
   try {
-    const response = await fetch("https://pawsome-walks.vercel.app/walks", {
+    const response = await fetch("https://pawsome-walks.vercel.app/api/walks", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
