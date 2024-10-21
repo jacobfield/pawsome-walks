fc845aac195d1be7e4de81a136c3cb2197e852a1
jacobfield <jacobprogrammes@gmail.com>
2024-10-21 12:50:51 +0100

    loading feedback added

--- pawsome-walks/src/comps/Main.jsx
+++ pawsome-walks/src/comps/Main.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
+import { bouncy } from "ldrs";
+
+bouncy.register();
+
+// Default values shown
 
 export default function Main({ allWalks }) {
   const [walks, setWalks] = useState(
     Array.from({ length: 30 }, (_, i) => i + 1)
   );
-  //setWalks
+  const [loading, setLoading] = useState("Loading...");
+
+  if (!allWalks) {
+    return (
+      <div className="loadingContainer">
+        <l-bouncy size="45" speed="1.75" color="#64abc1"></l-bouncy>
+      </div>
+    );
+  }
   return (
     <section className="walksContainer">
       {allWalks &&
         allWalks.map((walk) => (
           <div key={walk.walkid} className="walk">
-            <h2>{walk.walkname}</h2>
+            <h2 className="walkPreviewTitle">{walk.walkname}</h2>
             <img
               className="walkPreviewImg"
               src={`walk-photos/${walk.photopath}.jpg`}
