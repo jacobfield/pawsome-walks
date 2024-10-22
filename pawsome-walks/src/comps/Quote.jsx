e6f02f71d2dc4f0906239c1a8ce8a1546a1a43cc
jacobfield <jacobprogrammes@gmail.com>
2024-10-22 11:50:44 +0100

    scrollbar update

--- pawsome-walks/src/comps/Quote.jsx
+++ pawsome-walks/src/comps/Quote.jsx
@@ -1,14 +1,14 @@
 export default function Quote() {
   const quotes = [
-    "Discover the most pawsome trails for you and your furry friend!",
+    "Discover Pawsome trails for you and your furry friend!",
     "Your go-to site for tail-wagging adventures!",
     "Find the perfect walk to make your dog's day!",
-    "Explore scenic routes for unforgettable dog-friendly outings!",
-    "Pawsome Walks: Where every step is an adventure for you and your dog!",
-    "Unleash happiness with walks that both you and your dog will love!",
-    "Find the best spots to walk, sniff, and explore with your canine companion!",
+    "Explore unforgettable outings!",
+    "Where every step is an adventure for you and your dog!",
+    "Walks that both you and your dog will love!",
+    "Find the best spots to walk, sniff, and explore !",
     "Your dog's new favorite site for outdoor fun!",
-    "Pawsome Walks: Creating memories, one walk at a time!",
+    "Creating memories, one walk at a time!",
     "The ultimate guide to exploring the great outdoors with your dog!",
     "The best place to find your new favourite walk!",
   ];
