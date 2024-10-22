0f40b0100edf4a29da41eb865fa1e40d2ec3dd37
jacobfield <jacobprogrammes@gmail.com>
2024-10-22 15:03:04 +0100

    walkdetail photopath working now

--- pawsome-walks/src/comps/WalkDetail.jsx
+++ pawsome-walks/src/comps/WalkDetail.jsx
@@ -1,28 +1,41 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import getWalkById from "../hooks/getWalkById";
+import { bouncy } from "ldrs";
 
 export default function WalkDetail() {
   const { walkid } = useParams(); // get walkId from params
   const [walk, setWalk] = useState(null);
+  bouncy.register();
 
   useEffect(() => {
     async function fetchWalk(walkid) {
       const walkData = await getWalkById(walkid);
       setWalk(walkData);
-      console.log("WalkDetail.jsx walk data", walkData);
+      console.log("WalkDetail.jsx walk data", walk);
     }
     fetchWalk(walkid);
-  }, []);
+  }, [walkid]);
 
+  if (!walk) {
+    return (
+      <div className="loadingContainer">
+        <l-bouncy size="45" speed="1.75" color="#64abc1"></l-bouncy>
+      </div>
+    );
+  }
+  console.log("walk", walk);
   return (
     <>
-      <p>Walk Detail</p>
+      <img
+        className=""
+        src={`/walk-photos/walk${walkid}.jpg`}
+        alt={walk.walkname}
+      />
       <div className="walkDetail">
         <Link className="noTextDecoration" to="/">
           Back to Walks
         </Link>
-        {/* Rest of the walk details */}
       </div>
     </>
   );
