6b47cfd488208a542a55511a54f41372aaa702cb
jacobfield <jacobprogrammes@gmail.com>
2024-10-22 14:30:20 +0100

    walk data retreived

--- pawsome-walks/src/comps/WalkDetail.jsx
+++ pawsome-walks/src/comps/WalkDetail.jsx
@@ -1,9 +1,20 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import getWalkById from "../hooks/getWalkById";
 
 export default function WalkDetail() {
   const { walkid } = useParams(); // get walkId from params
   const [walk, setWalk] = useState(null);
+
+  useEffect(() => {
+    async function fetchWalk(walkid) {
+      const walkData = await getWalkById(walkid);
+      setWalk(walkData);
+      console.log("WalkDetail.jsx walk data", walkData);
+    }
+    fetchWalk(walkid);
+  }, []);
+
   return (
     <>
       <p>Walk Detail</p>
