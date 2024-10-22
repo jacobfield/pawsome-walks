412aea20b367dcdb5063aea03eb3d5ae5ee714d9
jacobfield <jacobprogrammes@gmail.com>
2024-10-22 11:24:13 +0100

    updated quote

--- pawsome-walks/src/comps/Quote.jsx
+++ pawsome-walks/src/comps/Quote.jsx
@@ -2,10 +2,9 @@ export default function Quote() {
   return (
     <div className="quoteDiv">
       <h1 className="quote">
-        &ldquo;The best place to find your new favourite walk!&ldquo;
+        &ldquo; The best place to find your new favourite walk! &ldquo;
       </h1>
       <br></br>
     </div>
-
   );
 }
