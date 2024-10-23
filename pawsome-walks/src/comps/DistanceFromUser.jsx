17761e8c810e5bdbf196b8e8998b37e755633ffc
jacobfield <jacobprogrammes@gmail.com>
2024-10-23 16:26:32 +0100

    walk distce from user location added

--- pawsome-walks/src/comps/DistanceFromUser.jsx
+++ pawsome-walks/src/comps/DistanceFromUser.jsx
@@ -1,7 +1,25 @@
+import useGeolocation from "./google-maps/hooks/useGeolocation";
+import calculateDistance from "../hooks/calculateDistance";
 export default function DistanceFromUser({ walk }) {
+  const walkLat = walk.lat;
+  const walkLng = walk.lng;
+
+  const userCoordinates = useGeolocation(walkLat, walkLng);
+  const { latitude, longitude, success } = userCoordinates;
+  const userLat = latitude;
+  const userLng = longitude;
+
+  const distance = calculateDistance(walkLat, walkLng, userLat, userLng);
   return (
     <div className="onOfferContainer">
-      <h3 className="onOfferHeader">Distance from user:</h3>
+      <h3 className="onOfferHeader">{walk.walkname} is</h3>
+      <ul className="onOfferList">
+        <li>
+          <span className="distance">{distance}</span> miles
+        </li>
+        <br></br>
+        <li>from your current location</li>
+      </ul>
     </div>
   );
 }
