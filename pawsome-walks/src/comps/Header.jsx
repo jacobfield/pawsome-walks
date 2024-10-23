753721e7c9013384eccad1bbca840ce7bed8098b
jacobfield <jacobprogrammes@gmail.com>
2024-10-23 15:41:27 +0100

    added link to logo

--- pawsome-walks/src/comps/Header.jsx
+++ pawsome-walks/src/comps/Header.jsx
@@ -2,15 +2,18 @@ import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
 import { useContext } from "react";
 import { ThemeContext } from "./ThemeProvider";
+import { Link } from "react-router-dom";
 export default function Header() {
   const { darkTheme } = useContext(ThemeContext);
   return (
     <header className={`header fade ${darkTheme ? "dark" : "light"}`}>
-      <img
-        className={`logo ${darkTheme ? "dark" : "light"}`}
-        alt="Pawsome Walks Logo"
-        src="/logo.png"
-      ></img>
+      <Link className="noTextDecoration" to="/">
+        <img
+          className={`logo ${darkTheme ? "dark" : "light"}`}
+          alt="Pawsome Walks Logo"
+          src="/logo.png"
+        ></img>
+      </Link>
       <div className="searchBarContainer">
         <SearchBar></SearchBar>
       </div>
