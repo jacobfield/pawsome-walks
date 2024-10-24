475ba22918460f1abaa2f57dd7c4032f6f3801a9
jacobfield <jacobprogrammes@gmail.com>
2024-10-24 14:15:09 +0100

    exported main content to its own comp

--- pawsome-walks/src/App.jsx
+++ pawsome-walks/src/App.jsx
@@ -1,30 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import getAllWalks from "./hooks/apiCalls/getAllWalks.js";
-import Footer from "./comps/Footer.jsx";
-import WalkDetail from "./comps/WalkDetail.jsx";
+
 import ThemeProvider from "./comps/ThemeProvider.jsx";
 import AppContainer from "./comps/AppContainer.jsx";
-import Header from "./comps/Header.jsx";
-import Main from "./comps/Main.jsx";
+
 import "./App.css";
 import "./styles/small.css";
 import "./styles/medium.css";
 import "./styles/large.css";
 import "./styles/index.css";
 import { ThemeContext } from "./comps/ThemeProvider.jsx";
-import SignUp from "./comps/SignUp.jsx";
+import MainContent from "./comps/MainContent.jsx";
 
 function App() {
-  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
-
+  const { darkTheme } = useContext(ThemeContext);
   const [allWalks, setAllWalks] = useState();
 
   useEffect(() => {
     async function fetchWalksData() {
       const walksData = await getAllWalks();
       setAllWalks(walksData);
-      // console.log("App.jsx walks data", walksData);
     }
     fetchWalksData();
   }, []);
@@ -33,18 +29,7 @@ function App() {
     <ThemeProvider>
       <AppContainer>
         <Router>
-          <div className="app">
-            <Header />
-            <div className={`mainContent ${!darkTheme ? "icon-right" : ""}`}>
-              {/* // HERE */}
-              <Routes>
-                <Route path="/" element={<Main allWalks={allWalks} />} />
-                <Route path="/walk/:walkid" element={<WalkDetail />} />
-                <Route path="/signup" element={<SignUp />} />
-              </Routes>
-            </div>
-            <Footer />
-          </div>
+          <MainContent allWalks={allWalks} darkTheme={darkTheme} />
         </Router>
       </AppContainer>
     </ThemeProvider>
