803e85cdb16ef1ccad5d649f42320b39592b52aa
jacobfield <jacobprogrammes@gmail.com>
2024-10-25 16:24:27 +0100

    before making signIn context

--- pawsome-walks/src/comps/SignIn.jsx
+++ pawsome-walks/src/comps/SignIn.jsx
@@ -1,14 +1,21 @@
-import { useState, useContext } from "react";
-import postOwner from "../hooks/apiCalls/postOwner";
+import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ThemeContext } from "./ThemeProvider";
+import getAllOwners from "../hooks/apiCalls/getAllOwners";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
-
   const [password, setPassword] = useState("");
-
+  const [allOwners, setAllOwners] = useState();
   const { darkTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    async function fetchAllOwnersData() {
+      const allOwnersData = await getAllOwners();
+      setAllOwners(allOwnersData);
+    }
+    fetchAllOwnersData();
+  }, []);
 
   const handleEmailChange = (e) => setEmail(e.target.value);
 
@@ -38,8 +45,9 @@ export default function SignIn() {
     }
 
     try {
-      //   const ownerData = {email, password };
-      //    MAKE GET REQUEST TO CHECK AGAINST DATABASE HERE
+        if (allOwners.find((owner) => owner.email === email && owner.password === password)) {
+        // SET OWNER CONTEXT HERE
+        }
     } catch (error) {
       // Check for specific error code for unique constraint violations
 
@@ -76,7 +84,7 @@ export default function SignIn() {
         />
 
         <button type="submit">Sign In</button>
-      </form>{" "}
+      </form>
       <Link className="noTextDecoration" to="/SignUp">
         <p>First Time? {<br />} Create an account:</p>
       </Link>
@@ -86,3 +94,5 @@ export default function SignIn() {
 
 // once data is inputted, the user can click the "Sign Up" button to submit the form.
 // the form does a get request to check if email and password exist on a row in it
+// need to create some context that holds the logged in owner
+// if the email and password exist in the database, the user is logged in. logged in and owner is set to the owner context.
