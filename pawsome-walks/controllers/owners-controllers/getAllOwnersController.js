4544f3a0792ad60eac477f8b08091424c2b9d4ec
jacobfield <jacobprogrammes@gmail.com>
2024-10-25 15:49:18 +0100

    updated get all owners

--- pawsome-walks/controllers/owners-controllers/getAllOwnersController.js
+++ pawsome-walks/controllers/owners-controllers/getAllOwnersController.js
@@ -1,17 +1,19 @@
 import { owners } from "../../helper-functions/helperFunctions.js";
 
-export default async function getAllOwnersController(req, res) {
+export default async function getAllallOwnersController(req, res) {
   try {
-    const owners = await owners.getAllOwners();
+    const allOwners = await owners.getAllallOwners();
 
-    if (!owners) {
+    if (!allOwners) {
       return res
         .status(404)
-        .json({ status: "error", message: "No owners found" });
+        .json({ status: "error", message: "No allOwners found" });
     }
-    res.status(200).json({ status: "success", data: owners });
+    res.status(200).json({ status: "success", data: allOwners });
   } catch (error) {
-    console.error("Error in getAllOwnersController.js: ", error);
-    res.status(500).json({ status: "error", message: "Error getting owners" });
+    console.error("Error in getAllallOwnersController.js: ", error);
+    res
+      .status(500)
+      .json({ status: "error", message: "Error getting allOwners" });
   }
 }
