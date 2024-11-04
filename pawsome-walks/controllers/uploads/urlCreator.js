7f2a7c81157c1230df6ec6a8fd42edf645786263
jacobfield <jacobprogrammes@gmail.com>
2024-11-04 12:05:36 +0000

    http table updated

--- pawsome-walks/controllers/uploads/urlCreator.js
+++
@@ -1,9 +0,0 @@
-export default function urlCreator(filename) {
-  console.log(filename);
-  const url =
-    `https://gwinwlodpvympyoitnza.supabase.co/storage/v1/object/public/uploads/${filename}`.replace(
-      /\s/g,
-      ""
-    );
-  return url;
-}
