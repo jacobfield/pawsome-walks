f4391ea178b3fd52a082d0056f35b6127e7f14e3
jacobfield <jacobprogrammes@gmail.com>
2024-11-06 09:26:02 +0000

    created skeleton files for favourites feature

---
+++ pawsome-walks/src/hooks/apiCalls/getAllFavouriteWalksByOwnerId.js
@@ -0,0 +1,5 @@
+// in main, ifLoggedIn make api call to retrieve list of favourites, depending on ownerid
+// if walk is not in favourites, function to add it to favourites
+// if walk is in favourites, function to remove it from favourites
+// toggle these functions, depending on whether the walk is in favourites or not; if in, star button will remove it, if not, star button will add it
+// if user is not logged in, star button make a little alert thing telling user to log in for favourites feature
